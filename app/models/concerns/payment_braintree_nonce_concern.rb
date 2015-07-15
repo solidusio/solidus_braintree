a4d21d345139db2469c9a8a0cc7c9a75f6988116
Allie Larson <allisonlarson91@gmail.com>
2015-07-15 13:55:50 -0400

    decorators are loaded in engine

--- app/models/concerns/payment_braintree_nonce_concern.rb
+++ app/models/concerns/payment_braintree_nonce_concern.rb
@@ -1,20 +1,18 @@
-module SolidusBraintree
-  module PaymentBraintreeNonceConcern
-    extend ActiveSupport::Concern
-    included do
-      attr_accessor :payment_method_nonce
+module PaymentBraintreeNonceConcern
+  extend ActiveSupport::Concern
+  included do
+    attr_accessor :payment_method_nonce
 
-      prepend(InstanceMethods)
-    end
+    prepend(InstanceMethods)
+  end
 
-    module InstanceMethods
-      def create_payment_profile
-        return unless source.respond_to?(:has_payment_profile?) && !source.has_payment_profile?
+  module InstanceMethods
+    def create_payment_profile
+      return unless source.respond_to?(:has_payment_profile?) && !source.has_payment_profile?
 
-        payment_method.create_profile_from_nonce(self.order.user, self.order.bill_address, self.payment_method_nonce)
-      rescue ActiveMerchant::ConnectionError => e
-        gateway_error e
-      end
+      payment_method.create_profile_from_nonce(self.order.user, self.order.bill_address, self.payment_method_nonce)
+    rescue ActiveMerchant::ConnectionError => e
+      gateway_error e
     end
   end
 end
--- lib/solidus_braintree/engine.rb
+++ lib/solidus_braintree/engine.rb
@@ -7,5 +7,13 @@ module SolidusBraintree
     initializer "spree.gateway.payment_methods", after: "spree.register.payment_methods" do |app|
       app.config.spree.payment_methods << Solidus::Gateway::BraintreeGateway
     end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
+    config.to_prepare &method(:activate).to_proc
   end
 end
