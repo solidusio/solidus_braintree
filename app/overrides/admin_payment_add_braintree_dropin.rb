005f70b64990a6d1c8a54ba1540bfaf4c482207a
Allie Larson <allisonlarson91@gmail.com>
2015-07-16 17:24:28 -0400

    get client token and dropin ui in spree/admin

--- app/overrides/admin_payment_add_braintree_dropin.rb
+++ app/overrides/admin_payment_add_braintree_dropin.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: "spree/admin/payments/source_forms/_gateway",
   name: "admin_payment_add_braintree_dropin",
-  insert_bottom: "[data-id=credit-card]",
-  text: %Q{<%= javascript_include_tag "spree/backend/solidus_braintree", "data-turbolinks-track" => true, "crossorigin" => "anonymous" %><div class='braintree-dropin'></div>}
+  insert_bottom: "[data-hook=card_name]",
+  text: %Q{<%= javascript_include_tag "spree/backend/solidus_braintree", "data-turbolinks-track" => true, "crossorigin" => "anonymous" %><div id='braintree-dropin'></div>}
 )
