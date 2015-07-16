198821fbd799a21aee7c4efae82a80452f15f394
Jack Chu <jack@jackchu.com>
2015-07-16 17:56:40 -0400

    make dropin success submit the form

--- app/overrides/admin_payment_add_braintree_dropin.rb
+++ app/overrides/admin_payment_add_braintree_dropin.rb
@@ -2,5 +2,9 @@ Deface::Override.new(
   virtual_path: "spree/admin/payments/source_forms/_gateway",
   name: "admin_payment_add_braintree_dropin",
   insert_bottom: "[data-hook=card_name]",
-  text: %Q{<%= javascript_include_tag "spree/backend/solidus_braintree", "data-turbolinks-track" => true, "crossorigin" => "anonymous" %><div id='braintree-dropin'></div>}
+  text: %Q{
+    <%= javascript_include_tag "spree/backend/solidus_braintree", "data-turbolinks-track" => true, "crossorigin" => "anonymous" %>
+    <div id="braintree-dropin"></div>
+    <input type="hidden" id="payment_method_nonce" name="payment_source[<%= payment_method.id %>][payment_method_nonce]">
+  }
 )
