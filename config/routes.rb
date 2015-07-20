6cefdc576615f494135f2b52c1d8af10a25cf458
Jack Chu <jack@jackchu.com>
2015-07-20 17:42:11 -0400

    switch add_routes to routes.draw

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     resource  :payment_client_token, only: [:create], controller: 'braintree_client_token'
   end
