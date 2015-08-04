c88ec89dba6105bbc5d1bc00618378ad902ce2f4
Allie Larson <allisonlarson91@gmail.com>
2015-08-04 16:36:59 -0400

    ClientToken endpoint uses payment method id params
    When requesting a braintree client token in admin, will send the payment method id
    as params to find the gateway by. ClientTokenController will look up
    BraintreeGateway based on payment method id, if there is one. Otherwise
    will find the active gateway that matches the env.

--- app/controllers/spree/api/braintree_client_token_controller.rb
+++ app/controllers/spree/api/braintree_client_token_controller.rb
@@ -1,6 +1,11 @@
 class Spree::Api::BraintreeClientTokenController < Spree::Api::BaseController
   def create
-    gateway = Solidus::Gateway::BraintreeGateway.find_by!(active: true, environment: Rails.env)
+    if params[:payment_method_id]
+      gateway = Solidus::Gateway::BraintreeGateway.find!(params[:payment_method_id])
+    else
+      gateway = Solidus::Gateway::BraintreeGateway.find_by!(active: true, environment: Rails.env)
+    end
+
     render json: { client_token: gateway.generate_client_token, payment_method_id: gateway.id }
   end
 end
--- spec/controllers/spree/api/braintree_client_token_controller_spec.rb
+++ spec/controllers/spree/api/braintree_client_token_controller_spec.rb
@@ -2,23 +2,46 @@ require 'spec_helper'
 
 describe Spree::Api::BraintreeClientTokenController, :vcr, type: :controller do
   describe "POST create" do
-    before do
-      create_braintree_payment_method
-      post :create
+    context "with a payment method id" do
+      before do
+        gateway = create_braintree_payment_method
+        post :create, id: gateway.id
+      end
+
+      it "returns an http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns a content type of application/json" do
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it "returns a client_token" do
+        body = JSON.parse(response.body)
+        expect(body["client_token"]).to be_present
+        expect(body["payment_method_id"]).to be_present
+      end
     end
 
-    it "returns an http success" do
-      expect(response).to have_http_status(:success)
-    end
+    context "without a payment method id" do
+      before do
+        create_braintree_payment_method
+        post :create
+      end
 
-    it "returns a content type of application/json" do
-      expect(response.content_type).to eq("application/json")
-    end
+      it "returns an http success" do
+        expect(response).to have_http_status(:success)
+      end
 
-    it "returns a client_token" do
-      body = JSON.parse(response.body)
-      expect(body["client_token"]).to be_present
-      expect(body["payment_method_id"]).to be_present
+      it "returns a content type of application/json" do
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it "returns a client_token" do
+        body = JSON.parse(response.body)
+        expect(body["client_token"]).to be_present
+        expect(body["payment_method_id"]).to be_present
+      end
     end
   end
 end
