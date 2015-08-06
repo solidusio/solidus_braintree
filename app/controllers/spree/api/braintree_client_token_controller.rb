b5bb20eb5a7c5eb01a520657dfedcc20d47493c8
Allie Larson <allisonlarson91@gmail.com>
2015-08-06 15:43:46 -0400

    Throw exception if Gateway is not found

--- app/controllers/spree/api/braintree_client_token_controller.rb
+++ app/controllers/spree/api/braintree_client_token_controller.rb
@@ -1,7 +1,7 @@
 class Spree::Api::BraintreeClientTokenController < Spree::Api::BaseController
   def create
     if params[:payment_method_id]
-      gateway = Solidus::Gateway::BraintreeGateway.find!(params[:payment_method_id])
+      gateway = Solidus::Gateway::BraintreeGateway.find_by!(id: params[:payment_method_id])
     else
       gateway = Solidus::Gateway::BraintreeGateway.find_by!(active: true, environment: Rails.env)
     end
