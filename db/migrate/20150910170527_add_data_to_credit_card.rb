10df9ab1b3e22f3c621eaee910ff202285ac1b58
Diego Franciosi <dfranciosi@gmail.com>
2015-12-03 14:58:59 +0100

    Check MySQL version in spree_credit_cards data json field If not supported downgrade to a text field

--- db/migrate/20150910170527_add_data_to_credit_card.rb
+++ db/migrate/20150910170527_add_data_to_credit_card.rb
@@ -1,5 +1,15 @@
 class AddDataToCreditCard < ActiveRecord::Migration
   def change
-    add_column :spree_credit_cards, :data, :json
+    current_adapter_name    = ActiveRecord::Base.connection.adapter_name
+    if current_adapter_name != "Mysql2"
+      add_column :spree_credit_cards, :data, :json
+    else
+      current_adapter_version = Gem::Version.new(ActiveRecord::Base.connection.send(:version).join('.'))
+      if current_adapter_version >= Gem::Version::new('5.7.8') # supports_json?()
+        add_column :spree_credit_cards, :data, :json
+      else
+        add_column :spree_credit_cards, :data, :text
+      end
+    end
   end
 end
