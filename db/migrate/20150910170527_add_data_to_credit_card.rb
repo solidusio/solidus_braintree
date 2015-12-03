8675976877bb3b7d64d95bca52768aae3433e382
Diego Franciosi <dfranciosi@gmail.com>
2015-12-03 15:49:46 +0100

    Better version using edge ActiveRecord::ConnectionAdapters::AbstractAdapter#supports_json? method

--- db/migrate/20150910170527_add_data_to_credit_card.rb
+++ db/migrate/20150910170527_add_data_to_credit_card.rb
@@ -1,15 +1,9 @@
 class AddDataToCreditCard < ActiveRecord::Migration
   def change
-    current_adapter_name    = ActiveRecord::Base.connection.adapter_name
-    if current_adapter_name != "Mysql2"
+    if ActiveRecord::Base.connection.adapter_name != 'SQLite' && ActiveRecord::Base.connection.supports_json?
       add_column :spree_credit_cards, :data, :json
     else
-      current_adapter_version = Gem::Version.new(ActiveRecord::Base.connection.send(:version).join('.'))
-      if current_adapter_version >= Gem::Version::new('5.7.8') # supports_json?()
-        add_column :spree_credit_cards, :data, :json
-      else
-        add_column :spree_credit_cards, :data, :text
-      end
+      add_column :spree_credit_cards, :data, :text
     end
   end
 end
