60dbe6be156b921801ac5efbcc2712b8ca0b164b
Eric Saupe <ericsaupe@gmail.com>
2016-04-01 11:35:30 -0600

    Moved override into initializer

--- lib/generators/solidus_braintree/install/install_generator.rb
+++ lib/generators/solidus_braintree/install/install_generator.rb
@@ -3,6 +3,10 @@ module SolidusBraintree
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, type: :boolean, default: false
 
+      def add_javascripts
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/braintree/solidus_braintree\n"
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=solidus_braintree'
       end
