fa1ebc380accc2d8c848a1de16ee91069b3e1d61
John Hawthorn <john@stembolt.com>
2016-05-05 16:12:37 -0700

    Increase poltergeist timeout

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,11 @@ require 'spree/testing_support/url_helpers'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 require 'capybara/poltergeist'
+Capybara.register_driver(:poltergeist) do |app|
+  Capybara::Poltergeist::Driver.new app, timeout: 90
+end
 Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 10
 
 module SolidusGateway
   module Helpers
