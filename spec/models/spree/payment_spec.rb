cf6fd4c90ec25c1f9f6f453de9d521403b36b4f1
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-05-09 14:52:43 -0500

    Ensure device_data is added to the gateway_options hash.

--- spec/models/spree/payment_spec.rb
+++ spec/models/spree/payment_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
 describe Spree::Payment, type: :model do
-  let(:payment) { Spree::Payment.new }
+  let(:device_data){"{\"device_session_id\":\"75197918b634416368241bb8996b560c\",\"fraud_merchant_id\":\"600000\"}"}
+
   it "has a payment_method_nonce accessor" do
+    payment = Spree::Payment.new
+
     expect { payment.payment_method_nonce = "abc123" }.not_to raise_error
     expect(payment.payment_method_nonce).to eq("abc123")
+  end
+
+  it 'adds device_data from the associated order to gateway_options hash' do
+    payment = create(:payment)
+
+    payment.order.update_attribute(:braintree_device_data, device_data)
+    expect(payment.gateway_options[:device_data]).to be_present
   end
 end
