d27b8cde80109dfb15bf10d49a0dc107f0170843
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-13 17:25:28 +0200

    Use solidus_support to configure the test environment

---
+++ spec/support/vcr.rb
@@ -0,0 +1,9 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/cassettes"
+  c.hook_into :webmock
+  c.ignore_localhost = true
+  c.configure_rspec_metadata!
+  c.ignore_hosts 'chromedriver.storage.googleapis.com'
+end
