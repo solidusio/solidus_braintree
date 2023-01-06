4c11945d0da94442622c056bb7d4b93c4bf4b650
George Mendoza <gsmendoza@gmail.com>
2023-01-06 18:09:59 +0800

    Add optional SolidusPaypalBraintree alias
    Can be used by passing `require: 'solidus_paypal_braintree`
    directive to `gem 'solidus_braintree'` in Gemfile.

---
+++ lib/solidus_paypal_braintree.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'solidus_braintree'
+
+SolidusPaypalBraintree =
+  ActiveSupport::Deprecation::DeprecatedConstantProxy.new('SolidusPaypalBraintree', 'SolidusBraintree')
