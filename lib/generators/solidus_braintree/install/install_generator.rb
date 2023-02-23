c4e57d20a290f1681a3a5f52bca8e1c72c93be85
George Mendoza <gsmendoza@gmail.com>
2023-02-23 11:29:51 +0800

    Reorganize install generator based on PaypalCommercePlatform
    Copied from
    https://github.com/solidusio/solidus_paypal_commerce_platform/blob/a19184f26d8ff35cf467d233172763aa03e9fe8b/lib/generators/solidus_paypal_commerce_platform/install/install_generator.rb.

--- lib/generators/solidus_braintree/install/install_generator.rb
+++ lib/generators/solidus_braintree/install/install_generator.rb
@@ -8,11 +8,22 @@ module SolidusBraintree
       argument :app_path, type: :string, default: Rails.root
 
       class_option :migrate, type: :boolean, default: false
-      source_root File.expand_path('templates', __dir__)
+      class_option :backend, type: :boolean, default: true
+      class_option :frontend, type: :string, default: 'starter'
 
       # This is only used to run all-specs during development and CI,  regular installation limits
       # installed specs to frontend, which are the ones related to code copied to the target application.
       class_option :specs, type: :string, enum: %w[all frontend], default: 'frontend', hide: true
+
+      source_root File.expand_path('templates', __dir__)
+
+      def normalize_components_options
+        @components = {
+          backend: options[:backend],
+          starter_frontend: options[:frontend] == 'starter',
+          classic_frontend: options[:frontend] == 'classic',
+        }
+      end
 
       def add_test_gems
         gem_group :test do
@@ -24,7 +35,7 @@ module SolidusBraintree
         bundle_command 'install'
       end
 
-      def setup_initializer
+      def setup_initializers
         legacy_initializer_pathname =
           Pathname.new(destination_root).join('config/initializers/solidus_paypal_braintree.rb')
 
@@ -35,57 +46,13 @@ module SolidusBraintree
             "SolidusPaypalBraintree.configure do |config|\n",
             "SolidusBraintree.configure do |config|\n"
         else
-          template 'initializer.rb', 'config/initializers/solidus_braintree.rb'
+          directory 'config/initializers', 'config/initializers'
         end
       end
 
-      def setup_javascripts
-        inject_into_file 'vendor/assets/javascripts/spree/frontend/all.js',
-          "//= require jquery3\n",
-          before: '//= require rails-ujs',
-          verbose: true
-
-        gsub_file 'vendor/assets/javascripts/spree/frontend/all.js',
-          "//= require spree/frontend/solidus_paypal_braintree\n", ''
-
-        append_file 'app/assets/javascripts/solidus_starter_frontend.js',
-          "//= require spree/frontend/solidus_braintree\n"
-
-        gsub_file 'vendor/assets/javascripts/spree/backend/all.js',
-          "//= require spree/backend/solidus_paypal_braintree\n", ''
-
-        append_file 'vendor/assets/javascripts/spree/backend/all.js',
-          "//= require spree/backend/solidus_braintree\n"
-      end
-
-      def setup_stylesheets
-        gsub_file 'vendor/assets/stylesheets/spree/frontend/all.css',
-          " *= require spree/frontend/solidus_paypal_braintree\n", ''
-
-        inject_into_file 'app/assets/stylesheets/solidus_starter_frontend.css',
-          " *= require spree/frontend/solidus_braintree\n", before: %r{\*/}, verbose: true
-
-        gsub_file 'vendor/assets/stylesheets/spree/backend/all.css',
-          " *= require spree/backend/solidus_paypal_braintree\n", ''
-
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css',
-          " *= require spree/backend/solidus_braintree\n", before: %r{\*/}, verbose: true
-      end
-
-      def inject_checkout_helper_to_frontend_controllers
-        inject_into_class 'app/controllers/checkouts_controller.rb',
-          'CheckoutsController',
-          "  helper SolidusBraintree::BraintreeCheckoutHelper\n\n",
-          verbose: true
-
-        inject_into_class 'app/controllers/carts_controller.rb',
-          'CartsController',
-          "  helper SolidusBraintree::BraintreeCheckoutHelper\n\n",
-          verbose: true
-      end
-
-      def add_migrations
+      def run_migrations
         rake 'railties:install:migrations FROM=solidus_braintree'
+        run 'bin/rails db:migrate' if options[:migrate]
       end
 
       def mount_engine
@@ -95,39 +62,93 @@ module SolidusBraintree
         route "mount SolidusBraintree::Engine, at: '/solidus_braintree'"
       end
 
-      def run_migrations
-        run_migrations = options[:migrate] ||
-          ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+      def install_solidus_backend_support
+        support_code_for(:backend) do
+          gsub_file 'vendor/assets/javascripts/spree/backend/all.js',
+            "//= require spree/backend/solidus_paypal_braintree\n", ''
 
-        if run_migrations
-          rake 'db:migrate'
-        else
-          puts 'Skipping bin/rails db:migrate, don\'t forget to run it!' # rubocop:disable Rails/Output
+          append_file 'vendor/assets/javascripts/spree/backend/all.js',
+            "//= require spree/backend/solidus_braintree\n"
+
+          gsub_file 'vendor/assets/stylesheets/spree/backend/all.css',
+            " *= require spree/backend/solidus_paypal_braintree\n", ''
+
+          inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css',
+            " *= require spree/backend/solidus_braintree\n", before: %r{\*/}, verbose: true
         end
       end
 
-      def install_specs
-        spec_paths =
-          case options[:specs]
-          when 'all' then %w[spec]
-          when 'frontend'
-            %w[
-              spec/solidus_braintree_helper.rb
-              spec/system/frontend
-              spec/support
-            ]
-          end
+      def install_solidus_starter_frontend_support
+        support_code_for(:starter_frontend) do
+          inject_into_file 'vendor/assets/javascripts/spree/frontend/all.js',
+            "//= require jquery3\n",
+            before: '//= require rails-ujs',
+            verbose: true
 
-        spec_paths.each do |path|
-          if engine.root.join(path).directory?
-            directory engine.root.join(path), path
-          else
-            template engine.root.join(path), path
+          gsub_file 'vendor/assets/javascripts/spree/frontend/all.js',
+            "//= require spree/frontend/solidus_paypal_braintree\n", ''
+
+          append_file 'app/assets/javascripts/solidus_starter_frontend.js',
+            "//= require spree/frontend/solidus_braintree\n"
+
+          gsub_file 'vendor/assets/stylesheets/spree/frontend/all.css',
+            " *= require spree/frontend/solidus_paypal_braintree\n", ''
+
+          inject_into_file 'app/assets/stylesheets/solidus_starter_frontend.css',
+            " *= require spree/frontend/solidus_braintree\n", before: %r{\*/}, verbose: true
+
+          inject_into_class 'app/controllers/checkouts_controller.rb',
+            'CheckoutsController',
+            "  helper SolidusBraintree::BraintreeCheckoutHelper\n\n",
+            verbose: true
+
+          inject_into_class 'app/controllers/carts_controller.rb',
+            'CartsController',
+            "  helper SolidusBraintree::BraintreeCheckoutHelper\n\n",
+            verbose: true
+
+          spec_paths =
+            case options[:specs]
+            when 'all' then %w[spec]
+            when 'frontend'
+              %w[
+                spec/solidus_braintree_helper.rb
+                spec/system/frontend
+                spec/support
+              ]
+            end
+
+          spec_paths.each do |path|
+            if engine.root.join(path).directory?
+              directory engine.root.join(path), path
+            else
+              template engine.root.join(path), path
+            end
           end
+        end
+      end
+
+      def alert_no_classic_frontend_support
+        support_code_for(:classic_frontend) do
+          message = <<~TEXT
+            For solidus_frontend compatibility, please use the deprecated version 0.x.
+            The new version of this extension only supports Solidus Starter Frontend.
+            No frontend code has been copied to your application.
+          TEXT
+          say_status :error, set_color(message.tr("\n", ' '), :red), :red
         end
       end
 
       private
+
+      def support_code_for(component_name, &block)
+        if @components[component_name]
+          say_status :install, "[#{engine.engine_name}] solidus_#{component_name}", :blue
+          shell.indent(&block)
+        else
+          say_status :skip, "[#{engine.engine_name}] solidus_#{component_name}", :blue
+        end
+      end
 
       def engine
         SolidusBraintree::Engine
