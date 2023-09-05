03e3454712f8c38de48af414991f9c4a9fe90fb5
Daniele Palombo <387690+DanielePalombo@users.noreply.github.com>
2023-09-05 16:26:21 +0200

    Make this gem zeitwerk compatible
    Otherwise it gives error when autoloaded in the application

--- app/decorators/models/solidus_braintree/spree/credit_card_decorator.rb
+++ app/decorators/models/solidus_braintree/spree/credit_card_decorator.rb
@@ -1,3 +1,11 @@
-Spree::CreditCard.include SolidusBraintree::SkipRequireCardNumbersConcern
-Spree::CreditCard.include SolidusBraintree::AddNameValidationConcern
-Spree::CreditCard.include SolidusBraintree::UseDataFieldConcern
+# frozen_string_literal: true
+
+module CreditCardDecorator
+  def self.prepended(base)
+    base.include SolidusBraintree::SkipRequireCardNumbersConcern
+    base.include SolidusBraintree::AddNameValidationConcern
+    base.include SolidusBraintree::UseDataFieldConcern
+  end
+
+  Spree::CreditCard.prepend(self)
+end
--- app/decorators/models/solidus_braintree/spree/payment_decorator.rb
+++ app/decorators/models/solidus_braintree/spree/payment_decorator.rb
@@ -1,2 +1,10 @@
-Spree::Payment.include SolidusBraintree::PaymentBraintreeNonceConcern
-Spree::Payment.include SolidusBraintree::InjectDeviceDataConcern
+# frozen_string_literal: true
+
+module PaymentDecorator
+  def self.prepended(base)
+    base.include SolidusBraintree::PaymentBraintreeNonceConcern
+    base.include SolidusBraintree::InjectDeviceDataConcern
+  end
+
+  Spree::Payment.prepend(self)
+end
